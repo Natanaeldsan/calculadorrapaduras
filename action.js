9fc111faec5fa16df52a3a67a525abc6244bf2fb
Natanaeldsan <78664393+Natanaeldsan@users.noreply.github.com>
2021-09-20 19:39:08 -0300

    Add files via upload

--- action.js
+++ action.js
@@ -65,43 +65,43 @@ function limp() {
         document.getElementById('peso').value = '';
     }
 }
-var fcolor= '#FF8C00';
-var scolor='#008080';
-var tcolor= 'green';
-var focolor= 'red';
-var fivecolor= '#D3D3D3';
+var fcolor = '#FF8C00';
+var scolor = '#008080';
+var tcolor = 'green';
+var focolor = 'red';
+var fivecolor = '#D3D3D3';
 
 //funções
-function aTcolor(){
+function aTcolor() {
     container.style['backgroundColor'] = fcolor;
     container.style['color'] = 'white';
 }
-function bTcolor(){
+function bTcolor() {
     container.style['backgroundColor'] = scolor;
     container.style['color'] = 'white';
 }
-function cTcolor(){
+function cTcolor() {
     container.style['backgroundColor'] = tcolor;
     container.style['color'] = 'white';
-    }
-function dTcolor(){
+}
+function dTcolor() {
     container.style['backgroundColor'] = focolor;
     container.style['color'] = 'white';
 }
-function eTcolor(){
+function eTcolor() {
     container.style['backgroundColor'] = fivecolor;
     container.style['color'] = 'black';
 }
 
-acolor.style['backgroundColor']= fcolor;
-bcolor.style['backgroundColor']= scolor;
-ccolor.style['backgroundColor']= tcolor;
-dcolor.style['backgroundColor']= focolor;
-ecolor.style['backgroundColor']= fivecolor;
+acolor.style['backgroundColor'] = fcolor;
+bcolor.style['backgroundColor'] = scolor;
+ccolor.style['backgroundColor'] = tcolor;
+dcolor.style['backgroundColor'] = focolor;
+ecolor.style['backgroundColor'] = fivecolor;
 
-var tcor= container.style['backgroundColor'];
-if (tcor= '#7CFC00'){
-    container.style['color']='white';
+var tcor = container.style['backgroundColor'];
+if (tcor = '#7CFC00') {
+    container.style['color'] = 'white';
 }
 
 calcular.addEventListener('click', qtr);
